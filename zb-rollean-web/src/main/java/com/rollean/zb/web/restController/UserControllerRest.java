653825b52fa70ee7a11361577918244e6e05a3f6
rollean <95875549@qq.com>
2018-10-16 19:20:45 +0800

    init

--- zb-rollean-web/src/main/java/com/rollean/zb/web/restController/UserControllerRest.java
+++ zb-rollean-web/src/main/java/com/rollean/zb/web/restController/UserControllerRest.java
@@ -4,7 +4,6 @@ import com.rollean.zb.domain.User;
 import com.rollean.zb.service.repository.UserRepository;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
